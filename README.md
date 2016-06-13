3bce89ebae1da666888b741f6128ccb1127c0f9f
herve91 <herve.jouin@gmail.com>
2016-06-13 23:40:52 +0200

    Add files via upload
    Now, all the Archi components are correctly managed
    
    The plugin has been validated against SQLite, MySQL and PostGreSQL databases but not against Oracle, even if the driver is included.

--- README.md
+++ README.md
@@ -1,23 +1,21 @@
 # Archimate Tool Database-Plugin
 Database export/import plugin that store models in a central repository.
 
-The current version (v0.6) is able to :
+The current version is able to :
    - Export and import models to a database (PostGreSQL, MySQL, Oracle and SQLite drivers are included)
    - Version the models (keep an history on the model versions and allow to retreive a former version)
-   - Show up how many Archi components are exported and imported (kind of statistics)
+   - Show up how many Archi components are exported and imported (kind od statistics)
    - Filter the models listed on the import dialog (by element, relation or property)
-   - import models either in standalone mode (each in their own Archi model) or in shared mode (each in a separate subfolder of the same Archi model container, allwing to create relations between them). This part still needs improvements.
+   - import models either in standalone mode (each in their own Archi model) or in shared mode (all in the same Archi model, each in a separate subfolder)
 
 The current limitations are :
-   - All the Archi features are not yet managed (like sketches and images)
-   - Text fields (model purpose, components documentation, ...) are limited in length (use of varchars instead of blobs)
+   - Text fields (model purpose, components names, documentation, ...) are limited in length
+   - Models are independent (it is not yet possible to create relations between models)
 
 My roadmap includes the following functionalities :
-   - Of course, manage all Archi features (like sketches and images)
+   - Allow relations between models,
    - Manage locks (kind of check-in / check-out),
    - Manage credentials (read/only, read/write, administrator tasks, ...),
-   - Reduce import/export duration,
-   - Autiomatically generate view including a set of elements with all their dependencies (recursively) in order to allow impact analysis,
-   - Map objects to CMDB CIs (as my company uses ServiceNow as CMDB provider, I will rework my ServiceNow plugin),
+   - Map  objects to CMDB CIs (as my company uses ServiceNow as CMDB provider, I will rework my ServiceNow plugin),
    - etc ...
    
--- create tables (MySQL).sql
+++ create tables (MySQL).sql
@@ -1,234 +1,353 @@
-CREATE TABLE archimatediagrammodel
-(
-  id character varying(50) NOT NULL,
-  model character varying(50) NOT NULL,
-  version character varying(50) NOT NULL,
-  
-  connectionroutertype integer,
-  documentation character varying(4096),
-  folder character varying(255),
-  name character varying(255) NOT NULL,
-  type character varying(50),
-  viewpoint integer,
-  
-  CONSTRAINT pk_archimatediagrammodel PRIMARY KEY (id, model, version));
+SELECT 'You may need to increase the "max_allowed_packet" parameter depending on your images size';
 
-CREATE TABLE archimateelement
-(
-  id character varying(50) NOT NULL,
-  model character varying(50) NOT NULL,
-  version character varying(50) NOT NULL,
-  
-  documentation character varying(4096),
-  folder character varying(255),
-  name character varying(255) NOT NULL,
-  type character varying(50),
-  
-  CONSTRAINT pk_archimateelement PRIMARY KEY (id, model, version));
+CREATE TABLE archimatediagrammodel (
+	id VARCHAR (24) NOT NULL,
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	connectionroutertype INTEGER,
+	documentation VARCHAR (65535),
+	folder VARCHAR (255),
+	name VARCHAR (255) NOT NULL,
+	type VARCHAR (50),
+	viewpoint INTEGER,
+	
+	CONSTRAINT pk_archimatediagrammodel PRIMARY KEY (id, model, version)
+);
 
-CREATE TABLE canvasmodel
-(
-  id character varying(50) NOT NULL,
-  model character varying(50) NOT NULL,
-  version character varying(50) NOT NULL,
-  
-  documentation character varying(4096),
-  folder character varying(255),
-  name character varying(255),
-  hinttitle character varying(255),
-  hintcontent character varying(4096),
-  connectionroutertype integer,
-  
-  CONSTRAINT pk_canvasmodel PRIMARY KEY (id, model, version));
+CREATE TABLE archimateelement (
+	id VARCHAR (24) NOT NULL,
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	documentation VARCHAR (65535),
+	folder VARCHAR (255),
+	name VARCHAR (255) NOT NULL,
+	type VARCHAR (50),
+	
+	CONSTRAINT pk_archimateelement PRIMARY KEY (id, model, version)
+);
 
-CREATE TABLE canvasmodelblock
-(
-  id character varying(50) NOT NULL,
-  model character varying(50) NOT NULL,
-  version character varying(50) NOT NULL,
-  
-  parent character varying(255),
-  bordercolor character varying(255),
-  content character varying(4096),
-  fillcolor character varying(255),
-  font character varying(255),
-  fontcolor character varying(255),
-  hintcontent character varying(4096),
-  hinttitle character varying(255),
-  imagepath character varying(4096),
-  imageposition integer,
-  islocked boolean,
-  linecolor character varying(255),
-  linewidth integer,
-  name character varying(255),
-  textalignment integer,
-  textposition integer,
-  x integer,
-  y integer,
-  width integer,
-  height integer,
-  
-  indent integer,
-  rank integer,
-  
-  CONSTRAINT pk_canvasmodelblock PRIMARY KEY (id, model, version));
+CREATE TABLE bendpoint (
+parent VARCHAR (24) NOT NULL,
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	startx INTEGER,
+	starty INTEGER,
+	endx INTEGER,
+	endy INTEGER,
+	rank INTEGER,
+	
+	CONSTRAINT pk_bendpoint PRIMARY KEY (parent, model, version, rank)
+);
 
-CREATE TABLE canvasmodelsticky
-(
-  id character varying(50) NOT NULL,
-  model character varying(50) NOT NULL,
-  version character varying(50) NOT NULL,
-  
-  parent character varying(50),
-  bordercolor character varying(255),
-  content character varying(4096),
-  fillcolor character varying(255),
-  font character varying(255),
-  fontcolor character varying(255),
-  imagepath character varying(4096),
-  imageposition integer,
-  linecolor character varying(255),
-  linewidth integer,
-  notes character varying(4096),
-  name character varying(255),
-  source character varying(255),
-  target character varying(255),
-  textalignment integer,
-  textposition integer,
-  x integer,
-  y integer,
-  width integer,
-  height integer,
-  
-  indent integer,
-  rank integer,
-  
-  CONSTRAINT pk_canvasmodelsticky PRIMARY KEY (id, model, version));
+CREATE TABLE canvasmodel (
+	id VARCHAR (24) NOT NULL,
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	documentation VARCHAR (65535),
+	folder VARCHAR (255),
+	name VARCHAR (255),
+	hinttitle VARCHAR (255),
+	hintcontent VARCHAR (4096),
+	connectionroutertype INTEGER,
+	
+	CONSTRAINT pk_canvasmodel PRIMARY KEY (id, model, version)
+);
 
-CREATE TABLE diagrammodelarchimateconnection
-(
-  id character varying(50) NOT NULL,
-  model character varying(50) NOT NULL,
-  version character varying(50) NOT NULL,
-  
-  class character varying(255),
-  documentation character varying(4096),
-  linewidth integer,
-  font character varying(255),
-  fontcolor character varying(255),
-  linecolor character varying(255),
-  parent character varying(255) NOT NULL,
-  relationship character varying(255),
-  source character varying(255),
-  target character varying(255),
-  text character varying(255),
-  textposition character varying(255),
-  type integer,
-  
-  indent integer,
-  rank integer,
-  
-  CONSTRAINT pk_diagrammodelarchimateconnection PRIMARY KEY (id, model, version));
+CREATE TABLE canvasmodelblock (
+	id VARCHAR (24) NOT NULL,
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	parent VARCHAR (255),
+	bordercolor VARCHAR (255),
+	content VARCHAR (4096),
+	fillcolor VARCHAR (255),
+	font VARCHAR (255),
+	fontcolor VARCHAR (255),
+	hintcontent VARCHAR (4096),
+	hinttitle VARCHAR (255),
+	imagepath VARCHAR (4096),
+	imageposition INTEGER,
+	islocked boolean,
+	linecolor VARCHAR (255),
+	linewidth INTEGER,
+	name VARCHAR (255),
+	textalignment INTEGER,
+	textposition INTEGER,
+	indent INTEGER,
+	rank INTEGER,
+	x INTEGER,
+	y INTEGER,
+	width INTEGER,
+	height INTEGER,
+	
+	CONSTRAINT pk_canvasmodelblock PRIMARY KEY (id, model, version)
+);
 
-CREATE TABLE diagrammodelarchimateobject
-(
-  id character varying(50) NOT NULL,
-  model character varying(50) NOT NULL,
-  version character varying(50) NOT NULL,
-  
-  archimateelementid character varying(255),
-  archimateelementname character varying(255),
-  archimateelementclass character varying(255),
-  bordertype integer,
-  class character varying(255),
-  content character varying(4096),
-  documentation character varying(4096),
-  linecolor character varying(255),
-  linewidth integer,
-  font character varying(255),
-  fontcolor character varying(255),
-  fillcolor character varying(255),
-  name character varying(255),
-  parent character varying(255) NOT NULL,
-  targetconnections character varying(255),
-  textalignment integer,
-  type integer,
-  x integer,
-  y integer,
-  width integer,
-  height integer,
-  
-  indent integer,
-  rank integer,
+CREATE TABLE canvasmodelimage (
+	id VARCHAR (24) NOT NULL,
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	parent VARCHAR (50),
+	bordercolor VARCHAR (255),
+	islocked BOOLEAN,
+	fillcolor VARCHAR (255),
+	font VARCHAR (255),
+	fontcolor VARCHAR (255),
+	imagepath VARCHAR (4096),
+	linecolor VARCHAR (255),
+	linewidth INTEGER,
+	name VARCHAR (255),
+	targetconnections VARCHAR (255),
+	textalignment INTEGER,
+	indent INTEGER,
+	rank INTEGER,
+	x INTEGER,
+	y INTEGER,
+	width INTEGER,
+	height INTEGER,
+	
+	CONSTRAINT pk_canvasmodelimage PRIMARY KEY (id, model, version)
+);
 
-  CONSTRAINT pk_diagrammodelarchimateobject PRIMARY KEY (id, model, version));
+CREATE TABLE canvasmodelsticky (
+	id VARCHAR (24) NOT NULL,
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	parent VARCHAR (50),
+	bordercolor VARCHAR (255),
+	content VARCHAR (4096),
+	fillcolor VARCHAR (255),
+	font VARCHAR (255),
+	fontcolor VARCHAR (255),
+	imagepath VARCHAR (4096),
+	imageposition INTEGER,
+	islocked BOOLEAN,
+	linecolor VARCHAR (255),
+	linewidth INTEGER,
+	notes VARCHAR (4096),
+	name VARCHAR (255),
+	targetconnections VARCHAR (255),
+	textalignment INTEGER,
+	textposition INTEGER,
+	indent INTEGER,
+	rank INTEGER,
+	x INTEGER,
+	y INTEGER,
+	width INTEGER,
+	height INTEGER,
+	
+	CONSTRAINT pk_canvasmodelsticky PRIMARY KEY (id, model, version)
+);
 
-CREATE TABLE folder
-(
-  id character varying(50) NOT NULL,
-  model character varying(50) NOT NULL,
-  version character varying(50) NOT NULL,
-  
-  documentation character varying(4096),
-  parent character varying(50),
-  type integer,
-  name character varying(255),
-  
-  rank integer,
-  
-  CONSTRAINT pk_folder PRIMARY KEY (id, model, version));
+CREATE TABLE connection (
+	id VARCHAR (24) NOT NULL,
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	class VARCHAR (255),
+	documentation VARCHAR (65535),
+	islocked BOOLEAN,
+	linewidth INTEGER,
+	font VARCHAR (255),
+	fontcolor VARCHAR (255),
+	linecolor VARCHAR (255),
+	parent VARCHAR (255) NOT NULL,
+	relationship VARCHAR (255),
+	source VARCHAR (255),
+	target VARCHAR (255),
+	text VARCHAR (255),
+	textposition VARCHAR (255),
+	type INTEGER,
+	indent INTEGER,
+	rank INTEGER,
+	
+	CONSTRAINT pk_connection PRIMARY KEY (id, model, version)
+);
 
-CREATE TABLE model
-(
-  model character varying(50) NOT NULL,
-  version character varying(50) NOT NULL,
-  name character varying(255) NOT NULL,
-  
-  note character varying(255),
-  owner character varying(50),
-  period character varying(50),
-  purpose character varying(4096),
-  
-  CONSTRAINT pk_model PRIMARY KEY (model, version));
+CREATE TABLE diagrammodelarchimateobject (
+	id VARCHAR (24) NOT NULL,
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	archimateelementid VARCHAR (255),
+	archimateelementname VARCHAR (255),
+	archimateelementclass VARCHAR (255),
+	bordertype INTEGER,
+	class VARCHAR (255),
+	content VARCHAR (65535),
+	documentation VARCHAR (65535),
+	linecolor VARCHAR (255),
+	linewidth INTEGER,
+	font VARCHAR (255),
+	fontcolor VARCHAR (255),
+	fillcolor VARCHAR (255),
+	name VARCHAR (255),
+	parent VARCHAR (255) NOT NULL,
+	targetconnections VARCHAR (255),
+	textalignment INTEGER,
+	type INTEGER,
+	indent INTEGER,
+	rank INTEGER,
+	x INTEGER,
+	y INTEGER,
+	width INTEGER,
+	height INTEGER,
+	
+	CONSTRAINT pk_diagrammodelarchimateobject PRIMARY KEY (id, model, version)
+);
 
-CREATE TABLE bendpoint
-(
-  parent character varying(50) NOT NULL,
-  model character varying(50) NOT NULL,
-  version character varying(50) NOT NULL,
-  
-  startx integer,
-  starty integer,
-  endx integer,
-  endy integer,
-  
-  rank integer,
-  
-  CONSTRAINT pk_point PRIMARY KEY (parent, model, version, rank));
+CREATE TABLE diagrammodelreference (
+	id VARCHAR (24) NOT NULL,
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	fillcolor VARCHAR (255),
+	font VARCHAR (255),
+	fontcolor VARCHAR (255),
+	linecolor VARCHAR (255),
+	linewidth INTEGER,
+	parent VARCHAR (255) NOT NULL,
+	targetconnections VARCHAR (255),
+	textalignment INTEGER,
+	rank INTEGER,
+	indent INTEGER,
+	
+	CONSTRAINT pk_diagrammodelreference PRIMARY KEY (id, model, version)
+);
 
-CREATE TABLE property
-(
-  id character varying(50) NOT NULL,
-  parent character varying(50) NOT NULL,
-  model character varying(50) NOT NULL,
-  version character varying(50) NOT NULL,
-  
-  name character varying(4096) NOT NULL,
-  value character varying(4096),
+CREATE TABLE folder (
+	id VARCHAR (24) NOT NULL,
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	documentation VARCHAR (65535),
+	parent VARCHAR (24),
+	type INTEGER,
+	name VARCHAR (255),
+	rank INTEGER,
+	
+	CONSTRAINT pk_folder PRIMARY KEY (id, model, version)
+);
 
-  CONSTRAINT pk_property PRIMARY KEY (id, model, version, parent));
+CREATE TABLE images (
+	path VARCHAR (50),
+	md5 VARCHAR (32),
+	image MEDIUMBLOB,
+	
+	CONSTRAINT pk_images PRIMARY KEY (path)
+);
 
-CREATE TABLE relationship
-(
-  id character varying(50) NOT NULL,
-  model character varying(50) NOT NULL,
-  version character varying(50) NOT NULL,
-  
-  documentation character varying(4096),
-  name character varying(255) NOT NULL,
-  source character varying(50),
-  target character varying(50),
-  type character varying(50),
-  folder character varying(255),
-  
-  CONSTRAINT pk_relationship PRIMARY KEY (id, model, version)); 
+CREATE TABLE model (
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	name VARCHAR (255) NOT NULL,
+	note VARCHAR (255),
+	owner VARCHAR (50),
+	period VARCHAR (50),
+	purpose VARCHAR (65535),
+	countMetadatas INTEGER,
+	countFolders INTEGER,
+	countElements INTEGER,
+	countRelationships INTEGER,
+	countProperties INTEGER,
+	countArchimateDiagramModels INTEGER,
+	countDiagramModelArchimateObjects INTEGER,
+	countDiagramModelArchimateConnections INTEGER,
+	countDiagramModelGroups INTEGER,
+	countDiagramModelNotes INTEGER,
+	countCanvasModels INTEGER,
+	countCanvasModelBlocks INTEGER,
+	countCanvasModelStickys INTEGER,
+	countCanvasModelConnections INTEGER,
+	countCanvasModelImages INTEGER,
+	countSketchModels INTEGER,
+	countSketchModelActors INTEGER,
+	countSketchModelStickys INTEGER,
+	countDiagramModelConnections INTEGER,
+	countDiagramModelBendpoints INTEGER,
+	countDiagramModelReferences INTEGER,
+	countImages INTEGER,
+	
+	CONSTRAINT pk_model PRIMARY KEY (model, version)
+);
+
+CREATE TABLE property (
+	id VARCHAR (24) NOT NULL,
+	parent VARCHAR (24) NOT NULL,
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	name VARCHAR (4096) NOT NULL,
+	value VARCHAR (4096),
+	
+	CONSTRAINT pk_property PRIMARY KEY (id, model, version, parent)
+);
+
+CREATE TABLE relationship (
+	id VARCHAR (24) NOT NULL,
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	documentation VARCHAR (65535),
+	name VARCHAR (255) NOT NULL,
+	source VARCHAR (50),
+	target VARCHAR (50),
+	type VARCHAR (50),
+	folder VARCHAR (255),
+	
+	CONSTRAINT pk_relationship PRIMARY KEY (id, model, version)
+);
+
+CREATE TABLE sketchmodel (
+	id VARCHAR (24),
+	model VARCHAR (24),
+	version VARCHAR (11),
+	name VARCHAR (255),
+	documentation VARCHAR (65535),
+	connectionroutertype INTEGER,
+	background INTEGER,
+	folder VARCHAR (255),
+	
+	CONSTRAINT pk_sketchmodel PRIMARY KEY (id, model, version)
+);
+
+CREATE TABLE sketchmodelactor (
+	id VARCHAR (24) NOT NULL,
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	parent VARCHAR (255),
+	fillcolor VARCHAR (255),
+	font VARCHAR (255),
+	fontcolor VARCHAR (255),
+	linecolor VARCHAR (255),
+	linewidth INTEGER,
+	name VARCHAR (255),
+	targetconnections VARCHAR (255),
+	textalignment INTEGER,
+	indent INTEGER,
+	rank INTEGER,
+	x INTEGER,
+	y INTEGER,
+	width INTEGER,
+	height INTEGER,
+	
+	CONSTRAINT pk_sketchmodelactor PRIMARY KEY (id, model, version)
+);
+
+CREATE TABLE sketchmodelsticky (
+	id VARCHAR (24) NOT NULL,
+	model VARCHAR (24) NOT NULL,
+	version VARCHAR (11) NOT NULL,
+	parent VARCHAR (255),
+	content VARCHAR (4096),
+	fillcolor VARCHAR (255),
+	font VARCHAR (255),
+	fontcolor VARCHAR (255),
+	linecolor VARCHAR (255),
+	linewidth INTEGER,
+	name VARCHAR (255),
+	targetconnections VARCHAR (255),
+	textalignment INTEGER,
+	indent INTEGER,
+	rank INTEGER,
+	x INTEGER,
+	y INTEGER,
+	width INTEGER,
+	height INTEGER,
+	
+	CONSTRAINT pk_sketchmodelsticky PRIMARY KEY (id, model, version)
+);
