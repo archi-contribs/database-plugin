0e2af3b5f1f136a0e9fc4df2c6958c98b4e23542
herve91 <herve.jouin@gmail.com>
2018-02-02 14:21:36 +0100

    remove warnings

--- v2/sources/src/org/archicontribs/database/model/impl/canvas/CanvasModelBlock.java
+++ v2/sources/src/org/archicontribs/database/model/impl/canvas/CanvasModelBlock.java
@@ -17,13 +17,14 @@ public class CanvasModelBlock extends com.archimatetool.canvas.model.impl.Canvas
 	public CanvasModelBlock() {
 		super();
 		
-		dbMetadata = new DBMetadata(this);
+		this.dbMetadata = new DBMetadata(this);
 	}
 	
 	/**
 	 * Gets the DBMetadata of the object
 	 */
-	public DBMetadata getDBMetadata() {
-		return dbMetadata;
+	@Override
+    public DBMetadata getDBMetadata() {
+		return this.dbMetadata;
 	}
 }
