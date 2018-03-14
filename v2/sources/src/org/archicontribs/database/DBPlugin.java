f3a3d55138c1813f228df2c342ded7179bda28b6
herve91 <herve.jouin@gmail.com>
2018-03-14 17:38:40 +0100

    update release notes

--- v2/sources/src/org/archicontribs/database/DBPlugin.java
+++ v2/sources/src/org/archicontribs/database/DBPlugin.java
@@ -165,7 +165,8 @@ import org.eclipse.ui.preferences.ScopedPreferenceStore;
  * 										
  * v2.0.7b : 01/07/2017				Solve Neo4J errors
  * 
- * v2.1 : 11/03/2018				Bug fixes:
+ * v2.1 : 11/03/2018				Fill in the online help
+ *                                      Bug fixes:
  * 										Solve plugin initialization failure that occured some times
  *                                      Fix progress bar during download new version of the plugin from GitHub
  *                                      Reduce memory leak
