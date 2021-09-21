d805904b7746bde8518ecf05f430a909711aa1be
Herve Jouin <herve.jouin@gmail.com>
2021-09-21 21:48:05 +0200

    add javadoc

--- v2/sources/src/org/archicontribs/database/preferences/DBFileFieldEditor.java
+++ v2/sources/src/org/archicontribs/database/preferences/DBFileFieldEditor.java
@@ -16,18 +16,42 @@ import org.eclipse.swt.widgets.Composite;
  *
  */
 public class DBFileFieldEditor extends FileFieldEditor {
+    /**
+     * 
+     */
     public DBFileFieldEditor() {
     	super();
     }
     
+    /**
+     * Creates a new file field editor
+     * @param name
+     * @param labelText
+     * @param parent
+     */
     public DBFileFieldEditor(String name, String labelText, Composite parent) {
         super(name, labelText, false, parent);
     }
     
+    /**
+     * Creates a new file field editor
+     * @param name
+     * @param labelText
+     * @param enforceAbsolute
+     * @param parent
+     */
     public DBFileFieldEditor(String name, String labelText, boolean enforceAbsolute, Composite parent) {
         super(name, labelText, enforceAbsolute, VALIDATE_ON_FOCUS_LOST, parent);
     }
     
+    /**
+     * Creates a new file field editor
+     * @param name
+     * @param labelText
+     * @param enforceAbsolute
+     * @param validationStrategy
+     * @param parent
+     */
     public DBFileFieldEditor(String name, String labelText, boolean enforceAbsolute, int validationStrategy, Composite parent) {
     	super(name, labelText, enforceAbsolute, validationStrategy, parent);
     }
