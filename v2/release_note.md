7c1a14c7bffe29073b0739f3bd4fadc6837aa468
Herve Jouin <herve.jouin@gmail.com>
2021-09-22 14:52:33 +0200

    new version 4.8.2

--- v2/release_note.md
+++ v2/release_note.md
@@ -1,3 +1,6 @@
+v4.8.2: 22/09/2021
+* fix exception raised when model purpose is null rather than empty string
+
 v4.8.1: 22/09/2021
 * fix coherence checking
 * change plugin numbering to follow Archi's and indicate more clearly that this version is compatible with Archi 4.8 and not Archi 4.9
--- v2/sources/src/org/archicontribs/database/DBPlugin.java
+++ v2/sources/src/org/archicontribs/database/DBPlugin.java
@@ -381,6 +381,8 @@ import lombok.Getter;
  * 
  * v4.8.1   22/09/2021              fix coherence checking
  *                                  change plugin numbering to follow Archi's and indicate more clearly that this version is compatible with Archi 4.8 and not Archi 4.9
+ *                                  
+ * v4.8.2	22/09/2021				fix exception raised when model purpose is null rather than an empty string                                 
  * 
  * TO-DO list:
  * ----------
